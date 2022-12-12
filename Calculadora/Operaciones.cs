1957764dd889a8aade44c2c658af3902de83ac96
renzolyaniez <89863904+renzolyaniez@users.noreply.github.com>
2022-12-12 12:05:26 -0300

    12/12/2022

---
+++ Calculadora/Operaciones.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class Operaciones
+    {
+        public Operaciones()
+        {
+        }
+
+        public void suma()
+        {
+            double num1, num2;
+            double resultado;
+
+            Console.WriteLine("Operacion seleccionada: Suma\n\nIngrese un numero");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese otro numero para la suma\n");
+            num2 = int.Parse(Console.ReadLine());
+            resultado = num1 + num2;
+
+            Console.WriteLine(num1 + " + " + num2 + " = " + resultado);
+        }
+
+        public void resta()
+        {
+            double num1, num2;
+            double resultado;
+
+            Console.WriteLine("Operacion seleccionada: Resta\n\nIngrese un numero");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese otro numero para la resta\n");
+            num2 = int.Parse(Console.ReadLine());
+            resultado = num1 - num2;
+
+            Console.WriteLine(num1 + " - " + num2 + " = " + resultado);
+        }
+
+        public void multiplicacion()
+        {
+            double num1, num2;
+            double resultado;
+
+            Console.WriteLine("Operacion seleccionada: Multiplicacion\n\nIngrese un numero");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese otro numero para la multiplicacion\n");
+            num2 = int.Parse(Console.ReadLine());
+            resultado = num1 * num2;
+
+            Console.WriteLine(num1 + " * " + num2 + " = " + resultado);
+        }
+        public void division()
+        {
+            double num1, num2;
+            double resultado;
+
+            Console.WriteLine("Operacion seleccionada: División\n\nIngrese un numero");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese otro numero para la división\n");
+            num2 = int.Parse(Console.ReadLine());
+            if (num2 == 0)
+            {
+                Console.WriteLine("Error, no se puede dividir por 0");
+                return;
+            }
+            else
+            {
+                resultado = num1 / num2;
+
+                Console.WriteLine(num1 + " / " + num2 + " = " + resultado);
+            }
+        }
+    }
+}
---
+++ Calculadora/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int operacion = 0;
+            Operaciones op = new Operaciones();
+
+            do
+            {
+                Console.WriteLine("Calculadora\n");
+                Console.WriteLine("1- Suma\n2- Resta\n3- Multiplicación\n4- División\n5- Salir");
+
+                Console.WriteLine("Seleccione una operación:");
+                operacion = int.Parse(Console.ReadLine());
+
+                switch (operacion)
+                {
+                    case 1:
+                        op.suma();
+                        break;
+
+                    case 2:
+                        op.resta();
+                        break;
+
+                    case 3:
+                        op.multiplicacion();
+                        break;
+
+                    case 4:
+                        op.division();
+                        break;
+
+                    case 5:
+                        return;
+
+                    default:
+                        Console.WriteLine("Error, la opcion ingresada no es correcta\n");
+                        break;
+                }
+
+            } while (operacion != 5);
+            Console.ReadLine();
+        }
+    }
+}
